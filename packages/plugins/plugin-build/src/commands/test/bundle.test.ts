41bd52cf3e769ee1fdee617e2a8942c25fc6d2a3
Josh Kellendonk <joshkellendonk@gmail.com>
2021-10-22 11:08:22 -0600

    add platform-based tests

--- packages/plugins/plugin-build/src/commands/test/bundle.test.ts
+++ packages/plugins/plugin-build/src/commands/test/bundle.test.ts
@@ -1,14 +1,23 @@
 import { resolveNativePath } from "../bundle";
+import * as os from "os";
+import { npath, ppath } from "@yarnpkg/fslib";
 
 describe("resolveNativePath", () => {
-  const cases = [
-    ["C:\\foo\\bar", "/C:/foo/bar"],
-    ["/root/dir", "/root/dir"],
-  ];
+  const testWindows = os.platform().startsWith("win") ? test : test.skip;
+  const testNonWindows = !os.platform().startsWith("win") ? test : test.skip;
 
-  test.each(cases)("%s => %s", (given, expected) => {
-    const result = resolveNativePath(given);
+  testWindows("windows drive letter paths", () => {
+    const result = resolveNativePath("C:\\foo\\bar");
 
-    expect(result).toEqual(expected);
+    expect(result).toEqual("/C:/foo/bar");
+  });
+
+  testNonWindows("non-windows relative paths", () => {
+    const result = resolveNativePath("path/to/foo");
+
+    const cwd = npath.toPortablePath(process.cwd());
+    const subdir = npath.toPortablePath("path/to/foo");
+
+    expect(result).toEqual(ppath.join(cwd, subdir));
   });
 });
