7afdf36d278341a7503ca6131763851de07fbb11
Owen Kelly <owen@owenkelly.com.au>
2024-02-18 02:25:18 +1100

    prettier

--- packages/plugins/plugin-test/src/commands/test/supervisor.test.ts
+++ packages/plugins/plugin-test/src/commands/test/supervisor.test.ts
@@ -1,4 +1,4 @@
-import {formatTimestampDifference} from "@ojkelly/yarn-build-shared/src/supervisor";
+import { formatTimestampDifference } from "@ojkelly/yarn-build-shared/src/supervisor";
 
 test(`ensure formatTimestampDifference copes with a variety of time ranges`, () => {
   const from = 0;
@@ -8,19 +8,15 @@ test(`ensure formatTimestampDifference copes with a variety of time ranges`, () 
   const checkTwoMinutesSevenSeconds = 2 * 60 * 1000 + 7 * 1000;
   const checkTenMinutes = 10 * 60 * 1000;
 
-  expect(
-    formatTimestampDifference(from, checkFiveHundredMilliseconds)
-  ).toBe(`0.50s`);
-  expect(formatTimestampDifference(from, checkOneSecond)).toBe(
-    `1.00s`
+  expect(formatTimestampDifference(from, checkFiveHundredMilliseconds)).toBe(
+    `0.50s`,
   );
-  expect(
-    formatTimestampDifference(from, checkSixPointThreeSeconds)
-  ).toBe(`6.30s`);
-  expect(
-    formatTimestampDifference(from, checkTwoMinutesSevenSeconds)
-  ).toBe(`2m 7.00s`);
-  expect(formatTimestampDifference(from, checkTenMinutes)).toBe(
-    `10m`
+  expect(formatTimestampDifference(from, checkOneSecond)).toBe(`1.00s`);
+  expect(formatTimestampDifference(from, checkSixPointThreeSeconds)).toBe(
+    `6.30s`,
   );
+  expect(formatTimestampDifference(from, checkTwoMinutesSevenSeconds)).toBe(
+    `2m 7.00s`,
+  );
+  expect(formatTimestampDifference(from, checkTenMinutes)).toBe(`10m`);
 });
